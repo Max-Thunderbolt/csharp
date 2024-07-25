bea6c082fd7a089f0dabecb2805a289e66db1b21
Max van der Walt <127961157+MaxVanDerWalt@users.noreply.github.com>
2024-07-25 10:07:34 +0200

    WebApplication
    From a module named, Cloud Development. We were asked to make a market place type website

---
+++ KhumaloCraft/EmailSender.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Mail;
+using System.Net;
+
+namespace KhumaloCraft
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            var mail = "maxvanderwalt3@gmail.com";
+            var pw = "zygp igvp zhcc fjbc";
+
+            var client = new SmtpClient("smtp.gmail.com", 587)
+            {
+                EnableSsl = true,
+                Credentials = new NetworkCredential(mail, pw)
+            };
+
+            var mailMessage = new MailMessage(from: mail, to: email,
+                subject, htmlMessage)
+            {
+                IsBodyHtml = true
+            };
+            return client.SendMailAsync(mailMessage);
+        }
+    }
+}
---
+++ KhumaloCraft/IEmailSender.cs
@@ -0,0 +1,7 @@
+﻿namespace KhumaloCraft
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string body);
+    }
+}
